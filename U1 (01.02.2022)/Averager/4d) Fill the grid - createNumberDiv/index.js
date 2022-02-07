8772f4f0bfbbf3c9922927787d0fa34b2928fe66
anna-jonsson <anna.f.jonsson@gmail.com>
2022-02-07 13:54:32 +0100

    Finished 4d)

--- U1 (01.02.2022)/Averager/4d) Fill the grid - createNumberDiv/index.js
+++ U1 (01.02.2022)/Averager/4d) Fill the grid - createNumberDiv/index.js
@@ -8,10 +8,21 @@ as the HTML-content of that div (innerHTML) and returns the reference to the
 newly created div.
 
 Note that the newly created div must not be appended inside the function createNumberDiv.
-The must happen outside the function.
+The must happen outside the function. */
+
+function createNumberDiv () {
+   let e = document.createElement("div");
+   e.innerHTML = randomNumber(100);
+
+   return e;
+} 
+
+function randomNumber ( max ) {
+    return Math.floor( max * Math.random() );
+}
 
 
-TEST:
+/* TEST:
 document.querySelector("body").appendChild( createNumberDiv() )
 This must append a div with a random number to the body each time it is called.
 
